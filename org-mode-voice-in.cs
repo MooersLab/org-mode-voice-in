a125bd9dfa65ff26d5a9a5f3d7d924dd901015c5
Blaine Mooers <bmooers1@gmail.com>
2023-12-22 08:23:30 -0600

    Create org-mode-voice-in.csv

---
+++ org-mode-voice-in.cs
@@ -0,0 +1,35 @@
+to do item,<newline>*** TODO <capitalize>
+prioritized to do,<newline>*** TODO [#B] <capitalize>
+priority a,<newline>*** TODO [#A] <capitalize>
+priority a1,<newline>*** TODO [#A1] <capitalize>
+priority a2,<newline>*** TODO [#A2] <capitalize>
+priority a3,<newline>*** TODO [#A3] <capitalize>
+priority a4,<newline>*** TODO [#A4] <capitalize>
+priority a5,<newline>*** TODO [#A5] <capitalize>
+priority a6,<newline>*** TODO [#A6] <capitalize>
+priority a7,<newline>*** TODO [#A7] <capitalize>
+priority a8,<newline>*** TODO [#A8] <capitalize>
+priority a9,<newline>*** TODO [#A9] <capitalize>
+priority b,<newline>*** TODO [#B] <capitalize>
+priority b1,<newline> *** TODO [#B11] <capitalize>
+priority b2,<newline>*** TODO [#B12] <capitalize>
+priority b3,<newline>*** TODO [#B13] <capitalize>
+priority b4,<newline>*** TODO [#B14] <capitalize>
+priority b5,<newline>*** TODO [#B15] <capitalize>
+priority b6,<newline>*** TODO [#B16] <capitalize>
+priority b7,<newline>*** TODO [#B17] <capitalize>
+priority b8,<newline>*** TODO [#B18] <capitalize>
+priority b9,<newline>*** TODO [#B19] <capitalize>
+priority c,<newline>*** TODO [#C] <capitalize>
+priority c1,<newline>*** TODO [#C21] <capitalize>
+priority c2,<newline>*** TODO [#C22] <capitalize>
+priority c3,<newline>*** TODO [#C23] <capitalize>
+priority c4,<newline>*** TODO [#C24] <capitalize>
+priority c5,<newline>*** TODO [#C25] <capitalize>
+priority c6,<newline> ***TODO [#C26] <capitalize>
+priority c7,<newline>*** TODO [#C27] <capitalize>
+priority c8,<newline>*** TODO [#C28] <capitalize>
+priority c9,<newline>*** TODO [#C29] <capitalize>
+insert org file header,"#+title:  A simple org-mode document
+#+author: Blaine Mooers
+#+date:   20203-12-22"
